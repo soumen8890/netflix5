60d7ccb4ec45e1270e1ff5128dc37359227d7ee7
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2023-04-29 16:40:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Auto-Filter-V5-Without-UrlShortner.git /Auto-Filter-V5-Without-UrlShortner
+  git clone https://github.com/soumen8890/netflix5.git /netflix5
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-Filter-V5-Without-UrlShortner
+  git clone $UPSTREAM_REPO /netflix5
 fi
-cd /Auto-Filter-V5
+cd /netflix5
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
